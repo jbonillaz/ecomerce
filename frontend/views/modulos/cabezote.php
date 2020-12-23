01851907657b1cbf9f9e8a45a835e0bb035bf4c5
jbonillaz <damilan1991@gmail.com>
2020-12-23 10:25:37 -0500

    Terminando el Cabezote, etiquetas html5.

---
+++ frontend/views/modulos/cabezote.php
@@ -0,0 +1,252 @@
+<!--=====================================
+Top
+======================================-->
+
+<div class="container-fluid barraSuperior" id="top">
+
+    <div class="container">
+
+        <div class="row">
+                <!--=====================================
+                Top
+                ======================================-->
+            
+            <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+
+                <ul>
+                
+                    <li>
+                        <a href="http://facebook.com/" target="_blank"><i class="fa fa-facebook redSocial facebookBlanco" area-hiden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="http://youtube.com/" target="_blank"><i class="fa fa-youtube redSocial youtubeBlanco" area-hiden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href=" http://twitter.com/" target="_blank"><i class="fa fa-twitter redSocial twitterBlanco" area-hiden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="http://google.com/" target="_blank"><i class="fa fa-google-plus redSocial googleBlanco" area-hiden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="http://instagram.com/" target="_blank"><i class="fa fa-instagram redSocial instagramBlanco" area-hiden="true"></i></a>
+                    </li>
+
+
+                </ul>
+
+
+            </div>
+
+                <!--=====================================
+                Registro
+                ======================================-->
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+
+                <ul>
+
+                    <li><a href="#modalIngreso" data-toggle="modal">Ingresar</a></li>
+                    <li>|</li>
+                    <li><a href="#modalRegistro" data-toggle="modal">Crear una cuenta</a></li>
+                    
+                </ul>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+<!--=====================================
+Header
+======================================-->
+<header class="container-fluid">
+
+    <div class="container">
+
+        <div class="row" id="cabezote">
+
+                <!--=====================================
+                Registro
+                ======================================-->
+
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
+
+                <a href="#">
+
+                    <img src="http://localhost/proyectos/E-Comerce/frontend/views/img/plantilla/logo.png" class="img-responsive" alt="">
+                
+                </a>
+
+            </div>
+
+                <!--=====================================
+                Bloque de Categorías y Buscador.
+                ======================================-->
+
+            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+
+                    <!--=====================================
+                    Botonera de Categorias
+                    ======================================-->
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">
+
+                    <p>Categorías
+
+                        <span class="pull-right">
+
+                            <i class="fa fa-bars" aria-hidden="true"></i>
+                            
+                        </span>
+
+                    </p>
+
+                </div>
+
+                    <!--=====================================
+                    Buscador.
+                    ======================================-->
+
+                <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
+
+                    <input type="search" name="buscar" class="form-control" placeholder="Buscar...">
+
+                    <span class="input-group-btn">
+
+                        <a href="#">
+
+                            <button class="btn btn-default backColor" type="submit">
+
+                                <i class="fa fa-search"></i>
+                                
+                            </button>
+
+                        </a>
+
+                    </span>
+
+                </div>
+
+            </div>
+
+                <!--=====================================
+                Carito de Compras.
+                ======================================-->
+
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+
+                <a href="#">
+
+                    <button class="btn btn-default pull-left backColor">
+
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>  
+
+                    </button>
+
+                </a>
+
+                <p>Tu Cesta <span class="cantidadCesta">3</span> <br> USD $ <span class="sumaCesta">20</span> </p>
+
+            </div>
+
+                <!--=====================================
+                Categorías.
+                ======================================-->
+
+            <div class="col-xs-12 backColor" id="categorias">
+			
+                <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                    
+                    <h4>
+                        <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                    </h4>
+                    
+                    <hr>
+
+                    <ul>
+                        
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                                    
+                    </ul>
+
+                </div>	
+
+
+                <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                    
+                    <h4>
+                        <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                    </h4>
+                    
+                    <hr>
+
+                    <ul>
+                        
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                                    
+                    </ul>
+
+
+                </div>
+
+
+                <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                    
+                    <h4>
+                        <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                    </h4>
+                    
+                    <hr>
+
+                    <ul>
+                        
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                                    
+                    </ul>
+
+
+                </div>
+                	
+
+                <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                    
+                    <h4>
+                        <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                    </h4>
+                    
+                    <hr>
+
+                    <ul>
+                        
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                        <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                                    
+                    </ul>
+
+
+                </div>
+
+            
+            </div>	
+
+        </div>
+
+    </div>
+
+</header>
--- frontend/views/plantilla.php
+++ frontend/views/plantilla.php
@@ -8,16 +8,25 @@
 	<meta name="keyword" content="Lorem ipsum, dolor sit amet, consectetur, adipisicing, elit, Quisquam, accusantium, enim, esse">
     <title>Tienda Virtual</title>
     
-    <link rel="stylesheet" href="css/plugins/bootstrap.min.css">
-    <link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+    <!-- <base href="views/"> -->
+    <link rel="stylesheet" href="views/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="views/css/plugins/font-awesome.min.css">
 
-    <script src="js/plugins/jquery.min.js"></script>
-    <script src="js/plugins/bootstrap.min.js"></script>
+    <script src="views/js/plugins/jquery.min.js"></script>
+    <script src="views/js/plugins/bootstrap.min.js"></script>
 
 
 
 </head>
 <body>
+
+    <?php
+        /*=============================================
+		Cabezote principal
+        =============================================*/
+        
+        include "modulos/cabezote.php";
+    ?>
     
 </body>
 </html>
